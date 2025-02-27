751f4af4ae836c49e49d785631306d3b111ea969
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-02-27 03:51:11 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,40 @@ public class PracticeProblem {
 
 	}
 
+	public static int[] createIntArray() {
+		int[] intArray = new int[5];
 
+		for (int i = 0; i < 5; i++) {
+			intArray[i] = i + 1;
+		}
+		return intArray;
+	}
+
+	public static String[] createArray(String word, String word2, String word3, String word4) {
+		String[] stringArray = {word, word2, word3, word4};
+		return stringArray;
+	}
+
+	public static int findValue(int num, int[] intArray) {
+		for (int i = 0; i < intArray.length; i++) {
+			if (intArray[i] == num) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findThirdValue(String word, String[] stringArray) {
+		int count = 0;
+
+		for (int i = 0; i < stringArray.length; i++) {
+			if (stringArray[i].equals(word)) {
+				count++;
+				if (count == 3) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
 }
